23e7c54baa25338e9aff1fb57d62685cdb4f12d1
Glen Dambor <glenioborges@gmail.com>
2017-12-24 16:26:10 -0200

    front-end

---
+++ qr-cash-mobile/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Bem vindo ao Cartão-Solidário</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
