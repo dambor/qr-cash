1bd54c8b4d6c6e8dc69d0a9e18eede0ca3590b5b
Saulo Damasceno Borges <borges.saulo@gmail.com>
2017-12-24 12:12:44 -0200

    Initial project.

---
+++ qr-cash-common/src/main/java/br/com/dambor/qrcash/qrcashcommon/QrCashCommonApplication.java
@@ -0,0 +1,12 @@
+package br.com.dambor.qrcash.qrcashcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCashCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCashCommonApplication.class, args);
+	}
+}
