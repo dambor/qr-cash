e8611e9e312430199b5e218ed252c307ee3079a6
Saulo Damasceno Borges <borges.saulo@gmail.com>
2017-12-24 11:58:15 -0200

    Initial project.

---
+++ qr-cash-user-service/src/main/java/br/com/dambor/qrcash/userservice/QrCashUserServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.dambor.qrcash.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCashUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCashUserServiceApplication.class, args);
+	}
+}
---
+++ qr-cash-user-service/src/test/java/br/com/dambor/qrcash/userservice/QrCashUserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.dambor.qrcash.userservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QrCashUserServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
